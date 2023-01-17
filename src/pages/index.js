8a5d150f249672d19b8596f80d4950b2945ee892
Francesco Vigni <vignif@gmail.com>
2023-01-17 01:33:14 +0100

    social in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,11 @@ import { Seo } from "../components/seo"
 import Video from "../components/video"
 import { graphql } from "gatsby"
 
+import { useSiteMetadata } from "../hooks/use-site-metadata"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
 import handVideo from "../../data/videos/hand.mp4"
 import handShakeVideo from "../../data/videos/handshake.mp4"
 import carlaVideo from "../../data/videos/carla.mp4"
@@ -14,6 +19,8 @@ import teleVideo from "../../data/videos/telepresence.mp4"
 
 const IndexPage = ({ data }) => {
   // console.log(data)
+  const { social } = useSiteMetadata()
+  console.log(social)
   const news = data.allFile.nodes;
   // console.log(news);
   return (
@@ -26,7 +33,7 @@ const IndexPage = ({ data }) => {
             <p className="justify">
               Here I collect some of my achievements as well as some of my hobbies.
               I am a PhD student at the University of Naples Federico II, Italy where I study how robots can learn to interact in a human-like way.
-              As Early-stage Researcher within the MSCA <a href="http://www.perseo.eu/" target="_blank">perseo</a> project, I study how robots can exploit their social-cues in order to initiate interactions with humans.
+              As Early-stage Researcher within the MSCA <a href="http://www.perseo.eu/" target="_blank" rel="noreferrer">perseo</a> project, I study how robots can exploit their social-cues in order to initiate interactions with humans.
             </p>
           </div>
           <div className="col-sm-4 ms-auto">
@@ -39,35 +46,37 @@ const IndexPage = ({ data }) => {
               className="img-fluid profilepic"
             />
 
+
           </div>
+
         </div>
 
         <div className="row">
-          <div class="col-md-12">
-            <hr class="hr-text" data-content="Highlights" />
+          <div className="col-md-12">
+            <hr className="hr-text" data-content="Highlights" />
           </div>
         </div>
         <div className="row">
 
-          <div class="col-sm-3">
+          <div className="col-sm-3">
             <video className="embed-responsive embed-responsive-16by9 main-video img-fluid" controls muted={true} autoPlay={true} loop={true}>
               <source className="embed-responsive-item" src={handVideo} type="video/mp4" />
             </video>
             <p className="lead text-muted">Handshake prototype</p>
           </div>
-          <div class="col-sm-3">
+          <div className="col-sm-3">
             <video className="embed-responsive embed-responsive-16by9 main-video img-fluid" controls muted={true} autoPlay={true} loop={true}>
               <source className="embed-responsive-item" src={handShakeVideo} type="video/mp4" />
             </video>
             <p className="lead text-muted">@icra2019</p>
           </div>
-          <div class="col-sm-3">
+          <div className="col-sm-3">
             <video className="embed-responsive embed-responsive-16by9 main-video img-fluid" controls muted={true} autoPlay={true} loop={true}>
               <source className="embed-responsive-item" src={carlaVideo} type="video/mp4" />
             </video>
             <p className="lead text-muted">How do you <a href="https://github.com/vignif/carla-parking">park</a>?</p>
           </div>
-          <div class="col-sm-3">
+          <div className="col-sm-3">
             <video className="embed-responsive embed-responsive-16by9 main-video img-fluid" controls muted={true} autoPlay={true} loop={true}>
               <source className="embed-responsive-item" src={teleVideo} type="video/mp4" />
             </video>
@@ -82,7 +91,8 @@ const IndexPage = ({ data }) => {
         </div>
 
         {news.map((node) => (
-          <div className="row" key={node.id}>
+          <>
+          <div className="row" key={node.uniqueId}>
             <div className="col-sm-2 own_sub_container">
               <p className="own_date">{node.childMarkdownRemark.frontmatter.date}</p>
             </div>
@@ -91,18 +101,36 @@ const IndexPage = ({ data }) => {
               <div className="justify" dangerouslySetInnerHTML={{ __html: node.childMarkdownRemark.html }} />
             </div>
           </div>
-
+          </> 
         ))}
         <div className="row">
-          <div class="col-md-12">
-            <hr class="hr-text" data-content="Step by step" />
+          <div className="col-md-12">
+            <hr className="hr-text" data-content="Step by step" />
           </div>
         </div>
 
 
         <div className="row">
-          <div class="col-md-12">
-            <hr class="hr-text" data-content="Training" />
+          <div className="col-md-12">
+            <hr className="hr-text" data-content="Training" />
+          </div>
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="col-md-1">
+            <a href={social.github} target="_blank" rel="noreferrer" className="col-sm-2 mx-auto">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+          </div>
+          <div className="col-md-1">
+            <a href={social.twitter} target="_blank" rel="noreferrer" className="col-sm-2 mx-auto">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+          </div>
+          <div className="col-md-1">
+            <a href={social.linkedin} target="_blank" rel="noreferrer" className="col-sm-2 mx-auto">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
           </div>
         </div>
 
