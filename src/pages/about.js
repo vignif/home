18cff4a859e3b7af2f0b14fa9c2370b9f3be6221
Francesco Vigni <vignif@gmail.com>
2023-01-17 01:33:02 +0100

    social in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,15 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import { Seo } from "../components/seo"
 
-const AboutPage = () => (
+import { useSiteMetadata } from "../hooks/use-site-metadata"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+const AboutPage = () => {
+  const { social } = useSiteMetadata()
+  
+  return (
+
   <Layout>
     <section className="py-5 text-center container own_container">
       <div className="row py-lg-5">
@@ -29,10 +37,28 @@ const AboutPage = () => (
           <Link to="/">Go back to the homepage</Link>
         </div>
       </div>
+      <div className="row justify-content-center">
+          <div className="col-md-1">
+            <a href={social.github} target="_blank" rel="noreferrer" className="col-sm-2 mx-auto">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+          </div>
+          <div className="col-md-1">
+            <a href={social.twitter} target="_blank" rel="noreferrer" className="col-sm-2 mx-auto">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+          </div>
+          <div className="col-md-1">
+            <a href={social.linkedin} target="_blank" rel="noreferrer" className="col-sm-2 mx-auto">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+          </div>
+        </div>
     </section>
 
   </Layout>
 )
+}
 
 export default AboutPage
 
