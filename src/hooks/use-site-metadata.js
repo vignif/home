fe4f5a43ba003718bddc440b8ea1dc421c731367
Francesco Vigni <vignif@gmail.com>
2023-01-17 01:33:23 +0100

    social metadata

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -8,6 +8,11 @@ export const useSiteMetadata = () => {
             title
             description
             author
+            social{
+              twitter
+              github
+              linkedin
+            }
           }
       }
     }
