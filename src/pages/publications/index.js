f2246ad14bc46720d11ad1fbe12592d0d1961530
Francesco Vigni <vignif@gmail.com>
2023-02-03 23:55:02 +0100

    add icons and improve buttons in publications page

--- src/pages/publications/index.js
+++ src/pages/publications/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../../components/layout"
 import { Seo } from "../../components/seo"
+import { HiDocumentText, HiOutlineSearch } from 'react-icons/hi';
 
 const AltPublications = ({ data }) => {
   console.log(data)
@@ -22,49 +23,56 @@ const AltPublications = ({ data }) => {
           </div>
         </div>
         <div className="container own_sub_container">
-          
-            {publications.map(pub => {
-              // const date = pub.node.childMarkdownRemark.frontmatter.date;
-              // const title = pub.node.childMarkdownRemark.frontmatter.title;
-              return (
-                <div key={pub.id}>
-                  <>
 
-                    <div className="card m-3">
-                      <div className="row card-body p-2 blog_cards">
-                        {/* <div className="col-sm-2 own_sub_container">
+          {publications.map(pub => {
+            // const date = pub.node.childMarkdownRemark.frontmatter.date;
+            // const title = pub.node.childMarkdownRemark.frontmatter.title;
+            return (
+              <div key={pub.id}>
+                <>
+
+                  <div className="card m-3">
+                    <div className="row card-body p-2 blog_cards">
+                      {/* <div className="col-sm-2 own_sub_container">
                           <p className="own_date">{pub.date}</p>
                         </div> */}
 
-                        <div className="col-md-10">
-                          <p className="m-auto">{pub.title}</p>
-                          {
-                            pub.authors.map((author, index) => (
-                              console.log(author),
-                              console.log(index),
-                              console.log(pub.authors.length),
-                              <div key={author.id} className="authors_list">
+                      <div className="col-md-10">
+                        <p className="m-auto">{pub.title}</p>
+                        {
+                          pub.authors.map((author, index) => (
+                            console.log(author),
+                            console.log(index),
+                            console.log(pub.authors.length),
+                            <div key={author.id} className="authors_list fw-light">
 
-                                {/* // put a comma between authors_list */}
-                                {index > 0 && index < pub.authors.length - 1 && ", "}
-                                {index > 0 && index === pub.authors.length - 1 && " and "}
-                                <p className="authors_list fw-light">{author.name} <a href={author.web} target="_blank" rel="noreferrer">{author.surname}</a></p>
-                              </div>
-                            ))}
-                          <p className="m-auto fw-lighter">{pub.venue}</p>
-                        </div>
+                              {/* // put a comma between authors_list */}
+                              {index > 0 && index < pub.authors.length - 1 && ", "}
+                              {index > 0 && index === pub.authors.length - 1 && " and "}
+                              <p className="authors_list">{author.name} <a href={author.web} target="_blank" rel="noreferrer">{author.surname}</a></p>
+                            </div>
+                          ))}
+                        <p className="m-auto fw-lighter">{pub.venue}</p>
+                      </div>
 
-                        <div className="col-md-2 text-center">
-                          <Link to={`/publications/${pub.slug}`} className="btn btn-link">More Info
-                          </Link>
+                      <div className="col-md-2">
+                        <div className="row">
+                          <div className="col text-center">
+                            <a href={pub.url} target="_blank" rel="noreferrer" className="btn btn-outline-primary"><HiDocumentText />&nbsp;Paper</a>
+                          </div>
+                          <div className="col text-center">
+                            <Link to={`/publications/${pub.slug}`} className="btn btn-outline-primary me-2"><HiOutlineSearch />&nbsp;Info
+                            </Link>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </>
-                </div>
-              )
-            }
-            )}
+                  </div>
+                </>
+              </div>
+            )
+          }
+          )}
           <center>
 
             <Link to="/">Go back to the homepage</Link>
