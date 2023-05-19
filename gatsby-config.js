a48a20f40de7c3aa2c21ce473adb1aa27f104186
Francesco Vigni <vignif@gmail.com>
2023-05-19 17:03:10 +0200

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,16 +21,8 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-RVHZ9WBRHJ", // Google Analytics / GA
           "G-QHV4ZCMNJ9"
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
