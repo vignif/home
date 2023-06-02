db8da9057cf795c8693d9bbded322293c6ba81ad
Francesco Vigni <vignif@gmail.com>
2023-06-02 13:14:15 +0200

    Updates

---
+++ 1a48c3c1-79b6968c70bfdb2b7ef0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_vignif_home=self.webpackChunk_vignif_home||[]).push([[874],{7516:function(t,n,a){a.d(n,{zwU:function(){return i}});var e=a(4405);function i(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"}},{tag:"path",attr:{d:"m9 17 8-5-8-5z"}}]})(t)}}}]);
+//# sourceMappingURL=1a48c3c1-79b6968c70bfdb2b7ef0.js.map
