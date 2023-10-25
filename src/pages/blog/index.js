ddb495e51bd7c6e7773c28e11e7f1a406ee68384
Francesco Vigni <vignif@gmail.com>
2023-10-25 22:33:00 +0200

    init cards in blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -3,11 +3,14 @@ import { Link, graphql } from "gatsby"
 import Layout from "../../components/layout"
 import { Seo } from "../../components/seo"
 import { HiOutlineNewspaper } from 'react-icons/hi';
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
 
 const Blog = ({ data }) => {
   // console.log(data)
   const blog = data.allFile.edges
   // console.log(blog)
+
   return (
     <Layout>
 
@@ -23,38 +26,45 @@ const Blog = ({ data }) => {
           </div>
         </div>
         <div className="container own_sub_container">
-          {blog.map(pub => {
-            console.log(pub.node)
-            const date = pub.node.childMarkdownRemark.frontmatter.date;
-            const title = pub.node.childMarkdownRemark.frontmatter.title;
-            const subtitle = pub.node.childMarkdownRemark.frontmatter.subtitle;
-            return (
-              <div key={pub.node.id}>
-                <>
+          <div class="row">
+            {blog.map(pub => {
+              console.log(pub.node)
+              const date = pub.node.childMarkdownRemark.frontmatter.date;
+              const title = pub.node.childMarkdownRemark.frontmatter.title;
+              const subtitle = pub.node.childMarkdownRemark.frontmatter.subtitle;
 
-                  <div className="card m-3">
-                    <div className="row card-body p-2 blog_cards">
-                      <div className="col-md-10">
-                        <p className="m-auto">{title}</p>
-                        <p className="m-auto fw-light">{subtitle}</p>
-                        <p className="m-auto fw-lighter">{date}</p>
-                      </div>
+              const image = getImage(pub.node.childMarkdownRemark.frontmatter.img)
+              console.log(image)
+              return (
+                <div key={pub.node.id}>
+                  <>
+                    <div class="col-5">
+                      <div className="card">
+                        <div className="card-image">
+                          <GatsbyImage image={image} alt="" />
+                        </div>
+                        <div className="card-content">
+                          <div className="content">
+                            <a href="/mere-blog-theme/2019/09/04/fifth-post/">
+                              <p className="title is-4">{title}</p>
+                            </a>
+                            <p className="subtitle is-6">{subtitle} - {date}</p>
+                          </div>
+                          <div className="has-text-centered">
 
-                      <div className="col-lg-2">
-                        <div className="row">
-                          <div className="col text-center">
-                            <Link to={`/blog${pub.node.childMarkdownRemark.fields.slug}`} className="btn btn-outline-primary me-2"><HiOutlineNewspaper />&nbsp;Read
+                            <Link to={`/blog${pub.node.childMarkdownRemark.fields.slug}`} className="button is-dark is-fullwidth"><HiOutlineNewspaper />&nbsp;Read
                             </Link>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </>
-              </div>
-            )
-          }
-          )}
+                  </>
+                </div>
+
+              )
+            }
+            )}
+          </div>
           <center>
             <div className="row">
               <div className="col-md-12">
@@ -78,25 +88,30 @@ export const Head = () => (
 
 // export page query
 export const query = graphql`
-query MyQuery {
-  allFile(filter: {sourceInstanceName: {eq: "blog"}},
-  sort: {childrenMarkdownRemark: {frontmatter: {date: DESC}}}
-  ) {
-    edges {
-      node {
-        id
-        sourceInstanceName
-        childMarkdownRemark {
-          fields{
-            slug
-          }
-          frontmatter {
-            title
-            date(formatString: "DD MMM, YYYY")
-            subtitle
+  query BLOGS {
+    allFile(filter: {sourceInstanceName: {eq: "blog"}},
+    sort: {childrenMarkdownRemark: {frontmatter: {date: DESC}}}
+    ) {
+      edges {
+        node {
+          id
+          sourceInstanceName
+          childMarkdownRemark {
+            fields{
+              slug
+            }
+            frontmatter {
+              title
+              date(formatString: "DD MMM, YYYY")
+              subtitle
+              img{
+                childImageSharp {
+                  gatsbyImageData(width: 400, placeholder: BLURRED, quality: 90, formats: [AUTO, WEBP, AVIF])
+                }
+              }
+            }
           }
         }
       }
     }
-  }
-}`
+  }`
