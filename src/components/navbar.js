d7fc445cc8182b70f6957b5a7d8a0cb780305b63
Francesco Vigni <vignif@gmail.com>
2025-03-07 13:16:39 +0100

    improve navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,55 +1,63 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import * as React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
 
-const isActive = ({ isCurrent }) => {
-  return isCurrent ? { className: "nav-link active" } : { className: "nav-link" }
-}
+const isActive = ({ isCurrent }) => ({
+  className: `nav-link ${isCurrent ? "active position-relative" : ""}`,
+  style: isCurrent ? { borderBottom: "3px solid white", paddingBottom: "2px" } : {},
+});
 
-const ExactNavLink = props => (
-  <Link getProps={isActive} {...props} />
-)
+const ExactNavLink = ({ to, children }) => (
+  <Link to={to} getProps={isActive}>
+    {children}
+  </Link>
+);
 
-const Navbar = ({ siteTitle }) => {
-  return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-primary container">
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand" href="#">{siteTitle}</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-navbar"
-          aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+ExactNavLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
-        <div className="collapse navbar-collapse" id="main-navbar">
-          <ul className="navbar-nav ms-auto mb-2 mb-md-0">
-            <li className="nav-item">
-              <ExactNavLink to="/">Home</ExactNavLink>
+const NAV_ITEMS = [
+  { to: "/", label: "Home" },
+  { to: "/blog", label: "Blog" },
+  { to: "/publications", label: "Publications" },
+  { to: "/news", label: "News" },
+  { to: "/about", label: "About" },
+];
+
+const Navbar = ({ siteTitle = "" }) => (
+  <nav className="navbar navbar-expand-md navbar-dark bg-primary container">
+    <div className="container-fluid">
+      <Link to="/" className="navbar-brand">
+        {siteTitle}
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#main-navbar"
+        aria-controls="main-navbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="main-navbar">
+        <ul className="navbar-nav ms-auto mb-2 mb-md-0">
+          {NAV_ITEMS.map(({ to, label }) => (
+            <li className="nav-item" key={to}>
+              <ExactNavLink to={to}>{label}</ExactNavLink>
             </li>
-            <li className="nav-item">
-              <ExactNavLink to="/blog">Blog</ExactNavLink>
-            </li>
-            <li className="nav-item">
-              <ExactNavLink to="/publications">Publications</ExactNavLink>
-            </li>
-            <li className="nav-item">
-              <ExactNavLink to="/news">News</ExactNavLink>
-            </li>
-            <li className="nav-item">
-              <ExactNavLink to="/about">About</ExactNavLink>
-            </li>
-          </ul>
-        </div>
+          ))}
+        </ul>
       </div>
-    </nav>
-  )
-}
+    </div>
+  </nav>
+);
 
 Navbar.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
-Navbar.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Navbar
+export default Navbar;
