e4285eb45c1170e721ddb7c9a774804e8fbf0fad
Francesco Vigni <vignif@gmail.com>
2025-09-26 14:28:56 +0200

    tracking

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,7 +4,43 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+const React = require("react")
+
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents, setPreBodyComponents }) => {
   setHtmlAttributes({ lang: "en" })
+
+  // Matomo Analytics
+  setHeadComponents([
+    React.createElement("script", {
+      key: "matomo",
+      dangerouslySetInnerHTML: {
+        __html: `
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.francescovigni.com/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+        `.trim(),
+      },
+    }),
+  ])
+
+  // Optional noscript fallback pixel for Matomo
+  setPreBodyComponents([
+    React.createElement("noscript", { key: "matomo-noscript" },
+      React.createElement("p", null,
+        React.createElement("img", {
+          src: "https://analytics.francescovigni.com/matomo.php?idsite=1&rec=1",
+          style: { border: 0 },
+          alt: "",
+        })
+      )
+    ),
+  ])
 }
