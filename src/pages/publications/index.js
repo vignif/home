454fa3bcf158c442548274adf95feb0266ca86ec
Francesco Vigni <vignif@gmail.com>
2026-01-10 00:15:27 +0100

    streamline publication styles

--- src/pages/publications/index.js
+++ src/pages/publications/index.js
@@ -32,226 +32,140 @@ const Publications = ({ data }) => {
         <h1 className="fw-semibold">Publications</h1>
         <hr className="custom-hr" />
       </section>
-      <div className="row">
-        <div className="col-md-12">
-          <hr className="hr-text" data-content="Conferences" />
-        </div>
-      </div>
-      <div className="container own_sub_container">
-        {publications.map(pub => {
-          return (
-            <div key={pub.id}>
-              <>
-                <div className="card m-3">
-                  <div className="row card-body p-2 blog_cards">
-                    <div className="col-md-10">
-                      <p className="m-auto">{pub.title}</p>
-                      {pub.authors.map((author, index) => (
-                        <div key={author.id} className="authors_list fw-light">
-                          {/* // put a comma between authors_list */}
-                          {index > 0 && index <= 2 && pub.coFirstAuthors && "*"}
-                          {index > 0 && index < pub.authors.length - 1 && ", "}
-                          {index > 0 &&
-                            index === pub.authors.length - 1 &&
-                            " and "}
-                          <p className="authors_list">
-                            {author.name}{" "}
-                            <a
-                              href={author.web}
-                              target="_blank"
-                              rel="noreferrer"
-                            >
-                              {author.surname}
-                            </a>
-                          </p>
-                        </div>
-                      ))}
-                      <p className="m-auto fw-lighter">
-                        {pub.venue} - {pub.date}
-                      </p>
-                      <div className="skills-list mt-2">
-                        {(pub.tags || []).map(tag => (
-                          <Link key={tag} to={`/skills/${skillSlug(tag)}`} className="skill-badge" aria-label={`Skill: ${tag}`}>{tag}</Link>
-                        ))}
-                      </div>
+
+      <hr className="hr-text" data-content="Conferences" />
+      <div className="publications-list container">
+        {publications.map(pub => (
+          <div key={pub.id} className="pub-card card m-3">
+            <div className="pub-row row card-body p-2">
+              <div className="pub-main col-md-10">
+                <p className="pub-title m-auto">{pub.title}</p>
+                {pub.authors.map((author, index) => (
+                  <span key={author.id} className="pub-author fw-light">
+                    {index > 0 && index <= 2 && pub.coFirstAuthors && "*"}
+                    {index > 0 && index < pub.authors.length - 1 && ", "}
+                    {index > 0 && index === pub.authors.length - 1 && " and "}
+                    <span className="pub-author-name">
+                      {author.name}{" "}
+                      <a href={author.web} target="_blank" rel="noreferrer">
+                        {author.surname}
+                      </a>
+                    </span>
+                  </span>
+                ))}
+                <p className="pub-meta m-auto fw-lighter">
+                  {pub.venue} - {pub.date}
+                </p>
+                <div className="skills-list mt-2">
+                  {(pub.tags || []).map(tag => (
+                    <Link key={tag} to={`/skills/${skillSlug(tag)}`} className="skill-badge" aria-label={`Skill: ${tag}`}>{tag}</Link>
+                  ))}
+                </div>
+              </div>
+              <div className="pub-actions col-md-2">
+                <div className="pub-actions-row row">
+                  {pub.abstract == "TBD" ? (
+                    <div className="col text-center">
+                      <span className="btn disabled me-2">Not Yet Presented</span>
                     </div>
-
-                    <div className="col-md-2">
-                      <div className="row">
-                        {/* \\ if pub is "TBD" then don't show the button */}
-                        {pub.abstract == "TBD" && (
-                          <>
-                            <div className="col text-center">
-                              <a
-                                href={pub.url}
-                                target="_blank"
-                                rel="noreferrer"
-                                className="btn disabled me-2"
-                              >
-                                Not Yet Presented
-                              </a>
-                            </div>
-                          </>
-                        )}
-                        {pub.abstract != "TBD" && (
-                          <>
-                            <div className="col text-center">
-                              {/* <a href={pub.url} rel="noreferrer" className="btn btn-outline-primary"><HiDocumentText />&nbsp;Paper</a> */}
-
-                              <div>
-                                {pub.url !== "" ? (
-                                  <a
-                                    href={pub.url}
-                                    rel="noreferrer"
-                                    target="_blank"
-                                    className="btn btn-outline-primary"
-                                  >
-                                    <HiDocumentText />
-                                    &nbsp;Paper
-                                  </a>
-                                ) : (
-                                  <span className="btn btn-outline-primary disabled">
-                                    <HiDocumentText />
-                                    &nbsp;Paper
-                                  </span>
-                                )}
-                              </div>
-                            </div>
-                            <div className="col text-center">
-                              <Link
-                                to={`/publications/${pub.slug}`}
-                                className="btn btn-outline-primary me-2"
-                              >
-                                <HiOutlineSearch />
-                                &nbsp;Info
-                              </Link>
-                            </div>
-                          </>
+                  ) : (
+                    <>
+                      <div className="col text-center">
+                        {pub.url !== "" ? (
+                          <a href={pub.url} rel="noreferrer" target="_blank" className="btn btn-outline-primary">
+                            <HiDocumentText />&nbsp;Paper
+                          </a>
+                        ) : (
+                          <span className="btn btn-outline-primary disabled">
+                            <HiDocumentText />&nbsp;Paper
+                          </span>
                         )}
                       </div>
-                      <div className="skills-list mt-2">
-                        {(pub.tags || []).map(tag => (
-                          <Link key={tag} to={`/skills/${skillSlug(tag)}`} className="skill-badge" aria-label={`Skill: ${tag}`}>{tag}</Link>
-                        ))}
+                      <div className="col text-center">
+                        <Link to={`/publications/${pub.slug}`} className="btn btn-outline-primary me-2">
+                          <HiOutlineSearch />&nbsp;Info
+                        </Link>
                       </div>
-                    </div>
+                    </>
+                  )}
+                </div>
+                <div className="skills-list mt-2">
+                  {(pub.tags || []).map(tag => (
+                    <Link key={tag} to={`/skills/${skillSlug(tag)}`} className="skill-badge" aria-label={`Skill: ${tag}`}>{tag}</Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <hr className="hr-text" data-content="Miscellaneous" />
+      <div className="misc-list container">
+        {misc.map(pub => (
+          <div key={pub.id} className="pub-card card m-3">
+            <div className="pub-row row card-body p-2">
+              <div className="pub-main col-md-10">
+                <p className="pub-title m-auto">{pub.title}</p>
+                {pub.authors.map((author, index) => (
+                  <span key={author.id} className="pub-author fw-light">
+                    {index > 0 && index < pub.authors.length - 1 && ", "}
+                    {index > 0 && index === pub.authors.length - 1 && " and "}
+                    <span className="pub-author-name">
+                      {author.name}{" "}
+                      <a href={author.web} target="_blank" rel="noreferrer">
+                        {author.surname}
+                      </a>
+                    </span>
+                  </span>
+                ))}
+                <p className="pub-meta m-auto fw-lighter">
+                  {pub.type} - {pub.date}
+                </p>
+              </div>
+              <div className="pub-actions col-md-2">
+                <div className="pub-actions-row row">
+                  <div className="col text-center">
+                    {pub.attach && (
+                      <a href={pub.attach.publicURL} rel="noreferrer" target="_blank" className="btn btn-outline-primary">
+                        <HiDocumentText />&nbsp;Paper
+                      </a>
+                    )}
+                  </div>
+                  <div className="col text-center">
+                    <Link to={`/publications/${pub.slug}`} className="btn btn-outline-primary me-2">
+                      <HiOutlineSearch />&nbsp;Info
+                    </Link>
                   </div>
                 </div>
-              </>
+              </div>
             </div>
-          )
-        })}
+          </div>
+        ))}
       </div>
 
-      <div className="row">
-        <div className="col-md-12">
-          <hr className="hr-text" data-content="Miscellaneous" />
-        </div>
-      </div>
-
-      <div className="container own_sub_container">
-        {misc.map(pub => {
-          return (
-            <div key={pub.id}>
-              <>
-                <div className="card m-3">
-                  <div className="row card-body p-2 blog_cards">
-                    <div className="col-md-10">
-                      <p className="m-auto">{pub.title}</p>
-                      {pub.authors.map((author, index) => (
-                        <div key={author.id} className="authors_list fw-light">
-                          {/* // put a comma between authors_list */}
-                          {index > 0 && index < pub.authors.length - 1 && ", "}
-                          {index > 0 &&
-                            index === pub.authors.length - 1 &&
-                            " and "}
-                          <p className="authors_list">
-                            {author.name}{" "}
-                            <a
-                              href={author.web}
-                              target="_blank"
-                              rel="noreferrer"
-                            >
-                              {author.surname}
-                            </a>
-                          </p>
-                        </div>
-                      ))}
-                      <p className="m-auto fw-lighter">
-                        {pub.type} - {pub.date}
-                      </p>
-                    </div>
-
-                    <div className="col-md-2">
-                      <div className="row">
-                        <>
-                          <div className="col text-center">
-                            <div>
-                              {pub.attach && (
-                                <a
-                                  href={pub.attach.publicURL}
-                                  rel="noreferrer"
-                                  target="_blank"
-                                  className="btn btn-outline-primary"
-                                >
-                                  <HiDocumentText />
-                                  &nbsp;Paper
-                                </a>
-                              )}
-                            </div>
-                          </div>
-                          <div className="col text-center">
-                            <Link
-                              to={`/publications/${pub.slug}`}
-                              className="btn btn-outline-primary me-2"
-                            >
-                              <HiOutlineSearch />
-                              &nbsp;Info
-                            </Link>
-                          </div>
-                        </>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </>
-            </div>
-          )
-        })}
-      </div>
-
-      <div className="row">
-        <div className="col-md-12">
-          <hr className="hr-text" data-content="Tags" />
-        </div>
-      </div>
-
-      <div className="row pb-5">
+      <hr className="hr-text" data-content="Tags" />
+      <div className="tags-list row pb-5">
         <div className="col">
           {sortedTags.map(tagObj => (
-            <div key={tagObj.name} className="authors_list fw-light col-2">
+            <span key={tagObj.name} className="pub-tag authors_list fw-light col-2">
               <Link className="btn btn-warning m-2" to={`/skills/${skillSlug(tagObj.name)}`}>
                 {tagObj.name} ({tagObj.count})
               </Link>
-            </div>
+            </span>
           ))}
         </div>
-
         <div className="col-md-12 p-2">
           <Link to={`/projects/tags/`} className="btn btn-primary m-2">
             Project Tags
           </Link>
         </div>
       </div>
-      <div className="row">
-        <div className="col-md-12">
-          <hr className="hr-text" data-content="" />
-        </div>
-      </div>
 
-      <center>
+      <hr className="hr-text" />
+      <div className="text-center pb-4">
         <Link to="/">Go back to the homepage</Link>
-      </center>
+      </div>
     </Layout>
   )
 }
